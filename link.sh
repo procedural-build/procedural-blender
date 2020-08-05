d4f48c9d8adca952ea70469adaa83b32f5986919
Mark Pitman <mark@procedural.build>
2020-08-05 11:50:02 +0200

    Fixed bug in link.sh

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# For development only: 
+# For development only:
 # Symbollically link the source folder into your Blender addons folder.
 
-ln -s $PWD/src $HOME/.config/blender/2.83/scripts/addons
+ln -s $PWD/src $HOME/.config/blender/2.83/scripts/addons/procedural_compute
