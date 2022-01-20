a090c03375c3433c799bf1fa3e1af5ce24a5b991
Mark Pitman <mark@procedural.build>
2022-01-20 12:26:36 +0100

    Updated bash link script

--- link.sh
+++ link.sh
@@ -8,10 +8,22 @@ DEFAULTADDONPATH="$HOME/.config/blender/3.0/scripts/addons"
 read -p "Enter path to addon folder [$DEFAULTADDONPATH]:" USERADDONPATH
 
 LINKFOLDER=$DEFAULTADDONPATH
-if [-z $USERADDONPATH] then
+if [ ! -z "$USERADDONPATH" ]; then
     LINKFOLDER=$USERADDONPATH
 fi
-LINKFOLDER="$LINKFOLDER/procedural_compute"
+
+LINKFOLDER=$LINKFOLDER/procedural_compute
+
+if [ -d "$LINKFOLDER" ]; then
+    read -p "Directory already exists at path. Delete and re-link? (y/n) [y] " RELINK
+    RELINK=${RELINK:-y}
+    if [ "$RELINK" == "y" ]; then
+        rm -v $LINKFOLDER
+    else
+        echo "Doing nothing because linked folder already exists and you selected to not delete it"
+        exit 0
+    fi
+fi
 
 echo "Linking procedurl_compute folder to $LINKFOLDER for development"
 ln -s $PWD/procedural_compute $LINKFOLDER
